b18876f254009910723d40125a25329f6f83cc98
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-03 13:03:50 +0300

    Update testAXIOSElasticsearch.js

--- server/testAXIOSElasticsearch.js
+++ server/testAXIOSElasticsearch.js
@@ -19,7 +19,7 @@ const agent = new https.Agent({ rejectUnauthorized: false });
 const { Client, BaseConnection } = require("@elastic/elasticsearch");
 const { ALL } = require("dns");
 const client = new Client({
-  node: "https://stagiaire:Police2405$@192.168.0.19:9200",
+  node: "https://stagiaire:Police2405$.168.0.19:9200",
   tls: {
     ca: fs.readFileSync("./assets/http_ca.crt"),
     rejectUnauthorized: false,
